85b412c8a3d9c005cfd8404bdddd2c9f762de04a
Makstarr <maksi112/@>
2020-03-27 15:23:50 +0300

    Финал

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
     $('.tabs-item').click(function (e) { 
         e.preventDefault();
         this
-        $('.tabs-item-active').removeClass('tabs-item-active');
         $(this).toggleClass('tabs-item-active'); 
-        $('.bounce').removeClass('bounce')
+        $('.tada').removeClass('tada')
         if ($(this).hasClass('bookmark')) {
             $(this).toggleClass('tada')
         } else { $('.visible').removeClass('visible')
-        $($(this).attr('href')).toggleClass('visible')}
+        $($(this).attr('href')).toggleClass('visible')
+        $('.tabs-item-active').removeClass('tabs-item-active');}
 
     });
     $('.subscribe-button').click(function (e) { 
