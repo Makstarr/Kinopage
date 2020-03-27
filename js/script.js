40fe9cc9066411768d900c7cef0b49e63308bcc4
Makstarr <maksi112/@>
2020-03-27 15:26:15 +0300

    Финал

--- js/script.js
+++ js/script.js
@@ -2,14 +2,13 @@ $(document).ready(function () {
 
     $('.tabs-item').click(function (e) { 
         e.preventDefault();
-        this
-        $(this).toggleClass('tabs-item-active'); 
         $('.tada').removeClass('tada')
         if ($(this).hasClass('bookmark')) {
             $(this).toggleClass('tada')
-        } else { $('.visible').removeClass('visible')
-        $($(this).attr('href')).toggleClass('visible')
-        $('.tabs-item-active').removeClass('tabs-item-active');}
+        } else { 
+            $('.visible').removeClass('visible');
+            $($(this).attr('href')).toggleClass('visible');
+            $('.tabs-item-active').removeClass('tabs-item-active')}
 
     });
     $('.subscribe-button').click(function (e) { 
